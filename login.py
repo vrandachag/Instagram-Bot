622d4fbc09fa884cea189040163234c041b52a7a
Vranda Chag <65115155+vrandachag@users.noreply.github.com>
2020-11-22 18:18:57 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,9 +6,19 @@ from bs4 import BeautifulSoup as b
 import time
 
 class Login:
-	def __init__(self, driver, username, password):
+	def __init__(self, driver, username, password): #built-in function - called automatically
 		self.driver = driver
 		self.username = username
 		self.password = password
 	def signin(self):
-		driver.get('')
+		print('opening')
+		self.driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+		uid = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#loginForm > div > div:nth-child(1) > div > label > input')))
+		uid.click()
+		uid.send_keys(self.username)
+		pswd=self.driver.find_element_by_css_selector('#loginForm > div > div:nth-child(2) > div > label > input')
+		pswd.click()
+		pswd.send_keys(self.password)
+		btn = self.driver.find_element_by_css_selector('#loginForm > div > div:nth-child(3)')
+		btn.click()
+		time.sleep(10)
